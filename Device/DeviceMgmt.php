a48cbaa5ce62497624c1f442b6f0852e8be1c246
jerry_chang <39659664+Jerrychanglab@users.noreply.github.com>
2024-10-29 10:23:09 +0800

    Update DeviceMgmt.php

--- Device/DeviceMgmt.php
+++ Device/DeviceMgmt.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// 防止頁面緩存
+// 防止页面被缓存
 header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1
 header("Pragma: no-cache"); // HTTP 1.0
 header("Expires: 0"); // Proxies
